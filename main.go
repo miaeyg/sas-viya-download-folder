b53b1a2e7d07bb7bd0c6b5e3a27c261650fd6bc4
Eyal Gonen <eyal.gonen@miac.com>
2024-05-07 17:37:45 +0300

    Tidy up

--- main.go
+++ main.go
@@ -16,28 +16,31 @@ import (
 )
 
 func main() {
-	var hostname, clientid, clientsecret, path, limit, code, output string
+	var hostname, clientid, clientsecret, path, code, output string
 	flag.StringVar(&output, "o", "c:/temp", "Please enter the output path")
 	flag.StringVar(&hostname, "h", "", "Please enter the hostname")
 	flag.StringVar(&clientid, "ci", "", "Please enter a ClientID")
 	flag.StringVar(&path, "path", "", "Please enter a folder path")
-	flag.StringVar(&limit, "l", "1", "Please enter a limit on the returned number of results")
 	flag.Parse()
 
-	// open browser to get authorization code
+	if path == "" {
+		log.Panicln("-path is a required parameter. Aborting.")
+	}
+
+	// Open browser to get authorization code
 	browser.OpenURL(hostname + "/SASLogon/oauth/authorize?client_id=" + clientid + "&response_type=code")
 
-	// get authorization code
+	// Get authorization code from end-user assuming he copied it from the browser
 	fmt.Println("Enter code from browser:")
 	fmt.Scan(&code)
 
-	// get client secret
+	// Get client secret
 	fmt.Println("Enter client secret:")
 	fmt.Println("\033[8m") // Hide input
 	fmt.Scan(&clientsecret)
 	fmt.Println("\033[28m") // Show input
 
-	// get SAS OAuth token
+	// Get SAS OAuth token
 	ai := core.AuthInfo{
 		// Username:     username,
 		// Password:     password,
@@ -49,43 +52,47 @@ func main() {
 	baseURL := hostname
 	token := ai.GetToken(baseURL)
 
-	// store token and baseURL in a context
+	// Store SAS Viya OAuth token and baseURL in a new context
 	ctx := context.Background()
 	ctx = context.WithValue(ctx, "token", &token)
 	ctx = context.WithValue(ctx, "baseURL", baseURL)
 
-	// folders query see https://developer.sas.com/apis/rest/#making-an-api-call for details on query syntax
+	// Folders query see https://developer.sas.com/apis/rest/#making-an-api-call for details on query syntax
+	// Search for the specific folder as passed in the input flag
 	qfl := url.Values{}
 	qfl.Add("limit", "1")
-	if path != "" {
-		qfl.Add("path", path)
-	}
+	qfl.Add("path", path)
 
-	// members query - only files that contain ".sas"
+	// Members query
+	// Create a query that selects only files who's names end with .sas
 	qmem := url.Values{}
 	qmem.Add("filter", "and(eq(contentType, 'file'),endsWith(name, '.sas'))")
 
-	log.Println("Root output folder:", output)
+	log.Printf("Downloading to root folder: %s\n", output)
 	log.Println("Searching for folders in SAS Content...")
 
-	// get list of folders and loop over them
+	// Get folder object from SAS
 	folder := sasobjs.GetFolder(ctx, qfl)
 
-	// handle folder
-	log.Printf("Folder Name: %v Id: %v Members: %v\n", folder.Name, folder.ID, folder.MemberCount)
+	// Handle folder
+	// 1. Log the folder name
+	// 2. Create directory under the root directory
+	log.Printf("--> Folder Name: %v Id: %v Members: %v\n", folder.Name, folder.ID, folder.MemberCount)
 	err := os.Mkdir(output+"/"+folder.Name, 0750)
 	if err != nil {
-		fmt.Println("Error trying to create output dir", folder.Name)
+		log.Panicf("Error trying to create output dir %s\n", folder.Name)
 	}
 
-	// get folder members that follow the applied member filter
+	// Get folder members that follow the applied member filter
+	// 1. Get the member's (file's) content
+	// 2. Save it to a new file in the directory created above
 	members := sasobjs.GetMembers(ctx, folder.ID, qmem)
 	for _, member := range members.Items {
-		log.Printf("Downloading member Name: %s Member URI: %s Member ID: %s\n", member.Name, member.URI, member.ID)
+		log.Printf("--> Downloading member Name: %s Member URI: %s Member ID: %s\n", member.Name, member.URI, member.ID)
 		memberContent := sasobjs.GetFileContent(ctx, member.URI)
 		file, err := os.Create(output + "/" + folder.Name + "/" + member.Name)
 		if err != nil {
-			fmt.Println("Error trying to create output file", member.Name)
+			log.Printf("Error trying to create output file %s\n", member.Name)
 			continue
 		}
 		io.Copy(file, bytes.NewReader(memberContent))
