9b44f67f934d3dba5deaccdc13bee9344af46967
Eyal Gonen <eyal.gonen@miac.com>
2024-05-08 10:56:46 +0300

    Set the stage for recursive call to handle subfolders

--- main.go
+++ main.go
@@ -16,8 +16,8 @@ import (
 )
 
 func main() {
-	var hostname, clientid, clientsecret, path, code, output string
-	flag.StringVar(&output, "o", "c:/temp", "Please enter the output path")
+	var hostname, clientid, clientsecret, path, code, rootOutputPath string
+	flag.StringVar(&rootOutputPath, "o", "c:/temp", "Please enter the output path")
 	flag.StringVar(&hostname, "h", "", "Please enter the hostname")
 	flag.StringVar(&clientid, "ci", "", "Please enter a ClientID")
 	flag.StringVar(&path, "path", "", "Please enter a folder path")
@@ -59,26 +59,28 @@ func main() {
 
 	// Folders query see https://developer.sas.com/apis/rest/#making-an-api-call for details on query syntax
 	// Search for the specific folder as passed in the input flag
-	qfl := url.Values{}
-	qfl.Add("limit", "1")
-	qfl.Add("path", path)
+	folderQuery := url.Values{}
+	folderQuery.Add("limit", "1")
+	folderQuery.Add("path", path)
 
 	// Members query
 	// Create a query that selects only files who's names end with .sas
-	qmem := url.Values{}
-	qmem.Add("filter", "and(eq(contentType, 'file'),endsWith(name, '.sas'))")
+	memberQuery := url.Values{}
+	memberQuery.Add("filter", "or(and(eq(contentType, 'file'),endsWith(name, '.sas')),eq(contentType, 'folder'))")
 
-	log.Printf("Downloading to root folder: %s\n", output)
+	log.Printf("Downloading to root folder: %s\n", rootOutputPath)
 	log.Println("Searching for folders in SAS Content...")
 
-	// Get folder object from SAS
-	folder := sasobjs.GetFolder(ctx, qfl)
+	downloadFolder(ctx, rootOutputPath, folderQuery, memberQuery)
+}
 
-	// Handle folder
-	// 1. Log the folder name
-	// 2. Create directory under the root directory
+// Handle download of a folder
+// 1. Log the folder name
+// 2. Create directory under the root directory
+func downloadFolder(ctx context.Context, basePath string, folderQuery url.Values, memberQuery url.Values) {
+	folder := sasobjs.GetFolder(ctx, folderQuery)
 	log.Printf("--> Folder Name: %v Id: %v Members: %v\n", folder.Name, folder.ID, folder.MemberCount)
-	err := os.Mkdir(output+"/"+folder.Name, 0750)
+	err := os.Mkdir(basePath+"/"+folder.Name, 0750)
 	if err != nil {
 		log.Panicf("Error trying to create output dir %s\n", folder.Name)
 	}
@@ -86,16 +88,23 @@ func main() {
 	// Get folder members that follow the applied member filter
 	// 1. Get the member's (file's) content
 	// 2. Save it to a new file in the directory created above
-	members := sasobjs.GetMembers(ctx, folder.ID, qmem)
+	members := sasobjs.GetMembers(ctx, folder.ID, memberQuery)
 	for _, member := range members.Items {
-		log.Printf("--> Downloading member Name: %s Member URI: %s Member ID: %s\n", member.Name, member.URI, member.ID)
-		memberContent := sasobjs.GetFileContent(ctx, member.URI)
-		file, err := os.Create(output + "/" + folder.Name + "/" + member.Name)
-		if err != nil {
-			log.Printf("Error trying to create output file %s\n", member.Name)
-			continue
+		switch member.ContentType {
+		case "file":
+			log.Printf("--> Downloading member Name: %s Member URI: %s Member ID: %s\n", member.Name, member.URI, member.ID)
+			memberContent := sasobjs.GetFileContent(ctx, member.URI)
+			file, err := os.Create(basePath + "/" + folder.Name + "/" + member.Name)
+			if err != nil {
+				log.Printf("Error trying to create output file %s\n", member.Name)
+				continue
+			}
+			io.Copy(file, bytes.NewReader(memberContent))
+			file.Close()
+		case "folder":
+			log.Printf("--> Found subfolder %s, skipping.", member.Name)
+		default:
+			log.Println("In type other")
 		}
-		io.Copy(file, bytes.NewReader(memberContent))
-		file.Close()
 	}
 }
